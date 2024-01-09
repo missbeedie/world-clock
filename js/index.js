18c541b63f54497b9ec3dd527ebc6b6d03485095
missbeedie <142709759+missbeedie@users.noreply.github.com>
2024-01-09 07:11:25 +0000

    added functionality for select

--- js/index.js
+++ js/index.js
@@ -20,5 +20,26 @@ parisTimeElement.innerHTML = parisCurrentTime.format('HH:mm:ss')
 
 }
 
+function updateCity(event){
+  let cityTimeZone = event.target.value
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1]
+  let cityTime = moment().tz(cityTimeZone)
+  let citiesElement = document.querySelector('#cities')
+  citiesElement.innerHTML = `
+  <div class="city">
+      <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format('dddd Do MMMM YYYY')}</div>
+      </div>
+      <div class="time">${cityTime.format('HH:mm:ss')}</div>
+    </div> 
+  `;
+}
+
 updateTime()
 setInterval(updateTime, 1000)
+
+
+
+let citiesSelectElement = document.querySelector('#city')
+citiesSelectElement.addEventListener('change', updateCity)
